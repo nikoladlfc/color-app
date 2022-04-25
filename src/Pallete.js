e946023e2656fc288dc76e12eac8a34a63e7a328
Nikola Draskovic <nikoladlfc@gmail.com>
2022-04-25 13:20:14 +0200

    Created react app, added pallete

---
+++ src/Pallete.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="Pallete">
+        {/* navigation */}
+
+        {/* Pallete Colors */}
+        <div className="Pallete-colors"></div>
+
+        {/* footer */}
+      </div>
+    );
+  }
+}
