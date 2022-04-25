de8fc04f46b746988f4be700e2598336b1363478
Nikola Draskovic <nikoladlfc@gmail.com>
2022-04-25 14:39:12 +0200

    Style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,9 +3,16 @@ import "./ColorBox.css";
 
 export default class ColorBox extends Component {
   render() {
+    const { name, background } = this.props;
+
     return (
-      <div style={{ background: this.props.background }} className="ColorBox">
-        <span>{this.props.name}</span>
+      <div style={{ background: background }} className="ColorBox">
+        <div className="copy-container"></div>
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">COPY</button>
+        <span className="see-more">MORE</span>
       </div>
     );
   }
