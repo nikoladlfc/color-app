c9696932d3c26cece4f6aad164b745b8dd50a39e
Nikola Draskovic <nikoladlfc@gmail.com>
2022-04-26 11:35:20 +0200

    Added color helpers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import seedColors from "./seedColors.js";
 import Pallete from "./Pallete.js";
+import { generatePalette } from "./colorHelpers.js";
 
 function App() {
+  console.log(generatePalette(seedColors[1]));
   return (
     <div className="App">
-      <Pallete {...seedColors[0]} />
+      <Pallete {...seedColors[6]} />
     </div>
   );
 }
