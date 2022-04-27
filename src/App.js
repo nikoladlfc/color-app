9f13f7518eaff337afd41fb38bf1b437d3874682
Nikola Draskovic <nikoladlfc@gmail.com>
2022-04-27 10:07:57 +0200

    Added react router @5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import seedColors from "./seedColors.js";
 import Palette from "./Palette.js";
 import { generatePalette } from "./colorHelpers.js";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedColors[1])} />
+      <Switch>
+        <Route path="/" render={() => <h1>Palette List</h1>} />
+        <Route path="/palette/:id" render={() => <h1>INDIVIDUAL</h1>} />
+      </Switch>
+      {/* <Palette palette={generatePalette(seedColors[1])} /> */}
     </div>
   );
 }
